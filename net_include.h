17bb03317e9c7ed2d1322a082125be5ffe37ca62
Chris Simmons <csimmo22@jhu.edu>
2016-10-08 11:52:10 -0400

    Update net_include.h

--- net_include.h
+++ net_include.h
@@ -15,4 +15,31 @@
 #define PORT	     10230
 
 #define MAX_MESS_LEN 1400
+#define RTR_SIZE 1380
+
+
+struct Token{
+    int index;
+    int token_index;
+    int aru;
+    int aru_id;
+    int num_rtr;
+    int rtr[RTR_SIZE];
+};
+
+struct Packet{
+    int index;
+    int random_number;
+    char payload_data[1300];
+};
+
+
+struct IP_Packet{
+    int p_id;
+    //double check correct type....
+    struct in_addr ip_addr;
+};
+
+
+
 
